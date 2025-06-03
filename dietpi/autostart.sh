784200863854ae5eff3efc316849a1d6906c9e87
Julian Hundeloh <5358638+jaulz@users.noreply.github.com>
2025-06-03 11:00:58 +0200

    feat: add dietpi configs

---
+++ dietpi/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "echo '' && echo '' && echo 'Please wait, MuPiBox-Installer starts soon...' && sleep 10" >> /home/dietpi/.bashrc
+echo "cd; curl -L https://raw.githubusercontent.com/splitti/MuPiBox/main/autosetup/autosetup.sh | bash" >> /home/dietpi/.bashrc
+
+sudo apt-get --yes install vim >&3 2>&3
+
+reboot
