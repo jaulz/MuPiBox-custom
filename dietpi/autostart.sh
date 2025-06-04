5b4687e560a0727b6dd672c8bc3ad0fc98e70afd
Julian Hundeloh <5358638+jaulz@users.noreply.github.com>
2025-06-04 08:36:42 +0200

    feat: add service

--- dietpi/autostart.sh
+++ dietpi/autostart.sh
@@ -3,6 +3,17 @@
 echo "echo '' && echo '' && echo 'Please wait, MuPiBox-Installer starts soon...' && sleep 10" >> /home/dietpi/.bashrc
 echo "cd; curl -L https://raw.githubusercontent.com/splitti/MuPiBox/main/autosetup/autosetup.sh | bash" >> /home/dietpi/.bashrc
 
+# Install required utilities
 sudo apt-get --yes install python3-evdev vim >&3 2>&3
+
+# Clone custom scripts
+git clone https://github.com/jaulz/MuPiBox-custom.git /home/dietpi/MuPiBox-custom
+
+# Set up buttons
+sudo apt-get --yes python3-evdev vim >&3 2>&3
+
+sudo cp -v /home/dietpi/MuPiBox-custom/services/mupi_buttons.service /etc/systemd/system/mupi_buttons.service
+sudo systemctl start mupi_buttons.service
+sudo systemctl enable mupi_buttons.service
 
 reboot
