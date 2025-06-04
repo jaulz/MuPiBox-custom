b1e442c66d16d73e051483f7cf7200a6a33e74a6
Julian Hundeloh <julian@hundeloh-consulting.ch>
2025-06-04 06:57:21 +0000

    fix: add missing install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,8 +6,9 @@ exec 3>/home/dietpi/.mupibox/autosetup.log; BASH_XTRACEFD=3; PS4=':$LINENO+'; se
 sudo apt-get --yes install vim >&3 2>&3
 
 # Set up buttons
-sudo apt-get --yes python3-evdev >&3 2>&3
+sudo apt-get --yes install python3-evdev >&3 2>&3
 
 sudo cp -v /home/dietpi/MuPiBox-custom/services/mupi_buttons.service /etc/systemd/system/mupi_buttons.service
+sudo systemctl daemon-reload
 sudo systemctl start mupi_buttons.service
 sudo systemctl enable mupi_buttons.service
