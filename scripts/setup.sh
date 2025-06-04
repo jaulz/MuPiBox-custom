6a4c4798abf0489a3dea6e2f3d11e4a463a2436c
Julian Hundeloh <julian@hundeloh-consulting.ch>
2025-06-04 18:47:43 +0000

    fix: output everything

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,10 +3,10 @@
 exec 3>/home/dietpi/.mupibox/autosetup.log; BASH_XTRACEFD=3; PS4=':$LINENO+'; set -x
 
 # Install required utilities
-sudo apt-get --yes install vim >&3 2>&3
+sudo apt-get --yes install vim 
 
 # Set up buttons
-sudo apt-get --yes install python3-evdev >&3 2>&3
+sudo apt-get --yes install python3-evdev
 
 sudo cp -v /home/dietpi/MuPiBox-custom/services/mupi_buttons.service /etc/systemd/system/mupi_buttons.service
 sudo systemctl daemon-reload
